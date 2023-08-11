c7e8eaa73d29587b205500ad39c9a98bccf40917
lordharamdes <ruel.d.tabernilla@gmail.com>
2023-08-10 20:33:07 -0400

    Finally!

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -82,16 +82,19 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 
 //Delete book review
+
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-  const { isbn } = req.params;
-  const { username } = req.session;
-  const book = books[isbn];
-  if (!book) {
-    return res.status(404).json({ message: "Book not found" });
-  }
-  book.reviews = book.reviews.filter((review) => review.username !== username);
-  return res.status(200).json({ message: "Review deleted" });
-});
+    const isbn = req.params.isbn;
+    let reviewer = req.session.authorization['username'];
+    let filtered_review = books[isbn]["reviews"];
+    if (filtered_review[reviewer]){
+        delete filtered_review[reviewer];
+        res.send(`Reviews for the ISBN  ${isbn} posted by the user ${reviewer} deleted.`);
+    }
+    else{
+        res.send("Can't delete, as this review has been posted by a different user");
+    }
+    });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
